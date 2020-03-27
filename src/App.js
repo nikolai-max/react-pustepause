6245ec8e69f8721da79ca9458ccea4e3249b11b1
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-03-27 14:57:32 +0100

    React new + copied code from my last pustepause project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import illustration from './images/illustration.png';
 import './App.css';
+import ModalAnt from './components/modalant'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <div className="explainer">
+          <div className="explainer-wrapper">En <span className="inline-color">Pustepause</span> er å finne roen inni seg selv og la <span className="inline-color">hodet</span> og <span className="inline-color">tankene</span> kunne <span className="inline-color">hvile...</span>
+          </div>
+        </div>
+        <div className="content-wrapper">
+          <img src={illustration} alt="Mann som sitter betenkt på en stol" className="illustration"></img>
+          <div>
+          <div className="inline-color">Registrer deg nå!</div>
+          <ModalAnt></ModalAnt>
+          </div>
+            
+        </div>
+        <div className="footer">
+          <div className="footer-wrapper">Endelig en meditasjonsapp på norsk. Regelmessig mindfullnessbasert stressreduksjon reduserer symptomer på angst og uro. <br></br><br></br>Utviklet av helsepersonell.</div>
+        </div>
+      </div>
     </div>
   );
 }
+        
 
 export default App;
