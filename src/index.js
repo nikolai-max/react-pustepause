dac824e59d8583b61feee2db353e238043358d00
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-04-08 09:35:04 +0200

    Testing to have all my index config in one file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('src', path.join(__dirname, 'src'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
